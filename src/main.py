8359b48732d9a029d01e8287a4237e1b6ac9cf47
zplato <zach.plato@gmail.com>
2024-03-24 07:23:59 -0400

    try simple endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def process_data():
+    print("HELLO WORLD!")
 
 
 def main():
-    print("HELLO WORLD!")
+    pass
 
 
 if __name__ == '__main__':
